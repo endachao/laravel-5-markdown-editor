71bf087768260ab5e55b01d50b6892e67f887fbb
yuanchao <653069653@qq.com>
2015-05-18 15:05:45 +0800

    1.0

---
+++ src/EndaEditor.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * 等待扩展
+ * User: 袁超<yccphp@163.com>
+ * Time: 2015.05.18 下午2:11
+ */
+namespace YuanChao\Editor;
+class EndaEditor{
+
+}
---
+++ src/config/editor.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    // 宽度
+    'width'=>'890px',
+    // 高度
+    'height'=>'500px',
+
+    'uploadUrl'=>'home/upload'
+];
